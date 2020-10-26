7779e1246cc636b16e38d87df895198a812f47eb
Mainkid <OsipovAA146@yandex.ru>
2020-10-26 10:47:44 +0500

    Source

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <iomanip>
+#include<Windows.h>
+#include <fstream>
+#include <ctime>
+#include "Matrix.h"
+#include "RootSearchMethods.h"
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	
+	return 0;
+}
