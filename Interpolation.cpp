8cf808b510c2fadfe4b8297c7938b3953473cd3f
Mainkid <OsipovAA146@yandex.ru>
2020-10-28 19:36:52 +0500

    Works Great for 19 variant

--- Interpolation.cpp
+++ Interpolation.cpp
@@ -7,54 +7,61 @@
 #include <cmath>
 
 const int width = 15;
+const double h = 0.2;
+const double a = 1;
+const double b = 2;
 
-void Interpolation::MakeDifTable (double a, double b, double step, int variant)
+const double c19=1;
+const double c20=0;
+const double c24 = 0;
+
+
+
+void Interpolation::MakeDifTable (double** difTable, double* xArray,double* difVector, int variant)
 {
 
 	std::cout << "Интерполяционная формула Ньютона" << std::endl;
 	std::cout << "Таблица разделенных разностей" << std::endl;
 	std::cout<<std::endl;
 
-	int n = (b-a) / step;
-	double* xArray = new double[n+1];
-	double* difVector = new double[n + 1];
+	int n = 5;
 
-	for (int i = 0; i < n+1; i++)
-	{
-		xArray[i] = a+step * (i);
-	}
 	double tmp;
+
 	for (int i=0; i<n+1;i++)
 		for (int j = 0; j < n + 2-i; j++)
 		{
 			if (j == 0)
+			{
 				std::cout << xArray[i] << " ";
+				difTable[i][j] = xArray[i];
+			}
 			else if (j==1)
 			{
 				
 				if (variant == 20)
 				{
-					tmp = F20(xArray[i]);
+					tmp = F20(xArray[i],0);
 					std::cout << tmp<< " ";
 				}
 				else if (variant == 24)
 				{
-					tmp = F24(xArray[i]);
+					tmp = F24(xArray[i],0);
 					std::cout << tmp << " ";
 				}
 				else if (variant == 19)
 				{
-					tmp = F19(xArray[i]);
+					tmp = F19(xArray[i],0);
 					std::cout << tmp << " ";
 
 				}
-				
+				difTable[i][j] = tmp;
 			}
 			else
 			{
-				tmp = RecF(i, j - 1 + i, xArray, variant);
+				tmp = RecF(i, j - 1 + i, xArray, variant, 0);
 				std::cout << tmp<< " ";
-
+				difTable[i][j] = tmp;
 
 			}
 			if (i == 0&&j>0)
@@ -64,16 +71,70 @@ void Interpolation::MakeDifTable (double a, double b, double step, int variant)
 			if (j == n+1-i)
 				std::cout << std::endl;
 		}
-
-
+	ShowDifTable(difTable, n + 1);
 	
-	NewtonInterpolation(difVector, n + 1,variant, a,b,n,xArray,n);
-	CubeSpline(xArray, difVector, n + 1, variant);
 }
 
-double Interpolation::F20(double x)
+void Interpolation::MakeDifTable(double** difTable, double* xArray, double* yArray, double* difVector, int variant)
 {
-	return pow(M_E, x) - pow(x, 2) + 1;
+	int n = 5;
+	double tmp=0;
+	double c = 0;
+	if (variant == 19)
+		c = c19;
+	else if (variant == 20)
+		c = c20;
+	else if (variant == 24)
+		c = c24;
+
+	for (int i = 0; i < n + 1; i++)
+		for (int j = 0; j < n + 2 - i; j++)
+		{
+			if (j == 1)
+			{
+				
+				difTable[i][j] = xArray[i];
+			}
+			else if (j == 0)
+			{
+
+				
+				difTable[i][j] = yArray[i];
+			}
+			else
+			{
+				tmp = RecF2(i, j - 1 + i, yArray,xArray, variant,c);
+
+				difTable[i][j] = tmp;
+
+			}
+			if (i == 0 && j > 0)
+				difVector[j - 1] = tmp;
+
+		}
+	ShowDifTable(difTable, n + 1);
+}
+
+void Interpolation::ShowDifTable(double** difTable, int n)
+{
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n+1-i; j++)
+		{
+			std::cout << " ";
+			std::cout << std::setw(12) << std::left << difTable[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+double Interpolation::F20(double x, double c)
+{
+	if (c == 0)
+		return pow(M_E, x) - pow(x, 2) + 1;
+	else
+		return  pow(M_E, x) - pow(x, 2) + 1 - c;
 }
 
 double Interpolation :: dF20(double x,int step)
@@ -87,9 +148,12 @@ double Interpolation::d1F20(double x)
 }
 
 
-double Interpolation::F24(double x)
+double Interpolation::F24(double x,double c)
 {
-	return 2 * pow(M_E, x) - 2 * x + 3;
+	if (c == 0)
+		return 2 * pow(M_E, x) - 2 * x + 3;
+	else
+		return 2 * pow(M_E, x) - 2 * x + 3-c;
 }
 
 double Interpolation :: dF24(double x, int step)
@@ -103,10 +167,12 @@ double Interpolation::d1F24(double x)
 	return 2 * pow(M_E, x) - 2;
 }
 
-double Interpolation::F19(double x)
+double Interpolation::F19(double x,double c)
 {
-	double new1 = pow(3, x) + 2 * x - 5;
+if (c==0)
 	return pow(3, x) + 2 * x - 5;
+else 
+	return pow(3, x) + 2 * x - 5-c;
 }
 
 double Interpolation::dF19(double x, int step)
@@ -119,28 +185,45 @@ double Interpolation::d1F19(double x)
 	return pow(3, x)*log(3) + 2;
 }
 
-double Interpolation::RecF(int start, int finish, double* Arr,int variant)
+double Interpolation::RecF(int start, int finish, double* Arr,int variant,double c)
 {
 	if (finish -start == 1)
 	{
 		if (variant == 20)
-			return (F20(Arr[finish]) - F20(Arr[start])) / (Arr[finish] - Arr[start]);
+			return (F20(Arr[finish],c) - F20(Arr[start],c)) / (Arr[finish] - Arr[start]);
 		else if (variant==24)
-			return (F24(Arr[finish]) - F24(Arr[start])) / (Arr[finish] - Arr[start]);
+			return (F24(Arr[finish],c) - F24(Arr[start],c)) / (Arr[finish] - Arr[start]);
 		else if (variant == 19)
 		{
-			double new1 = (F19(Arr[finish]) - F19(Arr[start])) / (Arr[finish] - Arr[start]);
-			return (F19(Arr[finish]) - F19(Arr[start])) / (Arr[finish] - Arr[start]);
+			double new1 = (F19(Arr[finish],c) - F19(Arr[start],c)) / (Arr[finish] - Arr[start]);
+			return (F19(Arr[finish],c) - F19(Arr[start],c)) / (Arr[finish] - Arr[start]);
 		}
 	}
 	else
-	{
+	{//????????????????
 		if (variant == 20)
-			return (F20(RecF(start + 1, finish, Arr, variant)) - F20(RecF(start, finish - 1, Arr, variant))) / (Arr[finish] - Arr[start]);
+			return (F20(RecF(start + 1, finish, Arr, variant,c),c) - F20(RecF(start, finish - 1, Arr, variant, c),c)) / (Arr[finish] - Arr[start]);
 		else if (variant==24)
-			return (F24(RecF(start + 1, finish, Arr, variant)) - F24(RecF(start, finish - 1, Arr, variant))) / (Arr[finish] - Arr[start]);
+			return (F24(RecF(start + 1, finish, Arr, variant,c),c) - F24(RecF(start, finish - 1, Arr, variant, c),c)) / (Arr[finish] - Arr[start]);
 		else if (variant==19)
-			return (RecF(start + 1, finish, Arr, variant) - RecF(start, finish - 1, Arr, variant)) / (Arr[finish] - Arr[start]);
+			return (RecF(start + 1, finish, Arr, variant, c) - RecF(start, finish - 1, Arr, variant, c)) / (Arr[finish] - Arr[start]);
+	}
+
+
+}
+
+double Interpolation::RecF2(int start, int finish, double* Arr, double* xArr,int variant, double c)
+{
+	if (finish - start == 1)
+	{
+			double new1 = (xArr[finish] - xArr[start]) / (Arr[finish] - Arr[start]);
+			return ((xArr[finish] - xArr[start]) / (Arr[finish] - Arr[start]));
+		
+	}
+	else
+	{//????????????????
+		
+			return (RecF2(start + 1, finish, Arr,xArr, variant, c) - RecF2(start, finish - 1, Arr,xArr, variant, c)) / (Arr[finish] - Arr[start]);
 	}
 
 
@@ -157,6 +240,20 @@ void Interpolation::PrintSplineHead()
 {
 std::cout<< std::setw(width) << std::left << "x" << std::setw(width) << std::left << "df/dx(x)" << std::setw(width) << std::left << "m" << std::setw(width) << std::left <<
 		"Delta" << std::setw(width) << std::left << "Оценка" << std::endl;
+
+}
+
+void Interpolation :: PrintSplineResultHead()
+{
+	std::cout << std::setw(width) << std::left << "x" << std::setw(width) << std::left << "f(x)" << std::setw(width) << std::left << "S31(f;x)" << std::setw(width) << std::left <<
+		"Delta" << std::setw(width) << std::left << "Оценка" << std::endl;
+
+}
+
+void Interpolation::PrintSplineResultBody(double x, double fx,double S31, double Delta, double Err)
+{
+	std::cout << std::setw(width) << std::left << x << std::setw(width) << std::left << fx << std::setw(width) << std::left << S31 << std::setw(width) << std::left <<
+		Delta << std::setw(width) << std::left << Err << std::endl;
 
 }
 
@@ -198,11 +295,11 @@ void Interpolation::NewtonInterpolation( double* difVector , int difVectorN, int
 	for (int i = 0; i < 5; i++)
 	{
 		if (variant == 19)
-			func = F19(xArray[i]);
+			func = F19(xArray[i],0);
 		else if (variant == 20)
-			func = F20(xArray[i]);
+			func = F20(xArray[i],0);
 		else if (variant == 24)
-			func = F24(xArray[i]);
+			func = F24(xArray[i],0);
 
 		Pn=CountPn(xArray[i], 5, Arr, nArr,difVector);
 		Rn = abs(FindPolynom(xArray[i], 6, Arr)*M6/720);
@@ -249,8 +346,9 @@ void Interpolation::CubeSpline(double* xArray, double* difVector, int N, int var
 	double M5;
 	
 	double* mVector = new double[N];
-
-
+	std::cout << std::endl;
+	std::cout << "Интерполяция кубическим сплайном" << std::endl;
+	std::cout<<std::endl;
 	if (variant == 19)
 	{
 		M5 = dF19(2, 5);
@@ -278,6 +376,17 @@ void Interpolation::CubeSpline(double* xArray, double* difVector, int N, int var
 
 
 
+}
+
+double Interpolation::Phi0(double tau)
+{
+	return (1 + 2 * tau)*(1 - tau)*(1 - tau);
+}
+
+double Interpolation::Phi1(double tau)
+{
+
+	return tau * (1 - tau)*(1 - tau);
 }
 
 void Interpolation::GetM(double* xArray, double* difVector, int N, int variant,double M5)
@@ -308,11 +417,11 @@ void Interpolation::GetM(double* xArray, double* difVector, int N, int variant,d
 			Matrix[0][0] = 2;
 			Matrix[0][1] = 0.5;
 			if (variant == 19)
-				Matrix[0][N - 2] = 1.5*(F19(xArray[i + 1]) - F19(xArray[i - 1])) / 0.2 - 0.5*d1F19(xArray[i - 1]);
+				Matrix[0][N - 2] = 1.5*(F19(xArray[i + 1],0) - F19(xArray[i - 1],0)) / 0.2 - 0.5*d1F19(xArray[i - 1]);
 			else if (variant == 20)
-				Matrix[0][N - 2] = 1.5*(F20(xArray[i + 1]) - F20(xArray[i - 1])) / 0.2 - 0.5*d1F20(xArray[i - 1]);
+				Matrix[0][N - 2] = 1.5*(F20(xArray[i + 1],0) - F20(xArray[i - 1],0)) / 0.2 - 0.5*d1F20(xArray[i - 1]);
 			else if (variant == 24)
-				Matrix[0][N - 2] = 1.5*(F24(xArray[i + 1]) - F24(xArray[i - 1])) / 0.2 - 0.5*d1F24(xArray[i - 1]);
+				Matrix[0][N - 2] = 1.5*(F24(xArray[i + 1],0) - F24(xArray[i - 1],0)) / 0.2 - 0.5*d1F24(xArray[i - 1]);
 		}
 		else if (i == N - 2)
 		{
@@ -320,11 +429,11 @@ void Interpolation::GetM(double* xArray, double* difVector, int N, int variant,d
 			Matrix[N - 3][N - 3] = 2;
 
 			if (variant == 19)
-				Matrix[N - 3][N - 2] = 1.5*(F19(xArray[i + 1]) - F19(xArray[i - 1])) / 0.2 - 0.5*d1F19(xArray[i + 1]);
+				Matrix[N - 3][N - 2] = 1.5*(F19(xArray[i + 1],0) - F19(xArray[i - 1],0)) / 0.2 - 0.5*d1F19(xArray[i + 1]);
 			else if (variant == 20)
-				Matrix[N - 3][N - 2] = 1.5*(F20(xArray[i + 1]) - F20(xArray[i - 1])) / 0.2 - 0.5*d1F20(xArray[i + 1]);
+				Matrix[N - 3][N - 2] = 1.5*(F20(xArray[i + 1],0) - F20(xArray[i - 1],0)) / 0.2 - 0.5*d1F20(xArray[i + 1]);
 			else if (variant == 24)
-				Matrix[N - 3][N - 2] = 1.5*(F24(xArray[i + 1]) - F24(xArray[i - 1])) / 0.2 - 0.5*d1F24(xArray[i + 1]);
+				Matrix[N - 3][N - 2] = 1.5*(F24(xArray[i + 1],0) - F24(xArray[i - 1],0)) / 0.2 - 0.5*d1F24(xArray[i + 1]);
 
 		}
 		else
@@ -337,14 +446,14 @@ void Interpolation::GetM(double* xArray, double* difVector, int N, int variant,d
 			{
 				double q = xArray[i + 1];
 				double m = xArray[i - 1];
-				Matrix[i - 1][N - 2] = 1.5*(F19(xArray[i + 1]) - F19(xArray[i - 1])) / 0.2;
+				Matrix[i - 1][N - 2] = 1.5*(F19(xArray[i + 1],0) - F19(xArray[i - 1],0)) / 0.2;
 				double tt = Matrix[i - 1][N - 2];
 				double weq;
 			}
 			else if (variant == 20)
-				Matrix[i - 1][N - 2] = 1.5*(F20(xArray[i + 1]) - F20(xArray[i - 1])) / 0.2;
+				Matrix[i - 1][N - 2] = 1.5*(F20(xArray[i + 1],0) - F20(xArray[i - 1],0)) / 0.2;
 			else if (variant == 24)
-				Matrix[i - 1][N - 2] = 1.5*(F24(xArray[i + 1]) - F24(xArray[i - 1])) / 0.2;
+				Matrix[i - 1][N - 2] = 1.5*(F24(xArray[i + 1],0) - F24(xArray[i - 1],0)) / 0.2;
 
 			j++;
 		}
@@ -462,8 +571,151 @@ void Interpolation::GetM(double* xArray, double* difVector, int N, int variant,d
 	{
 		M4 = dF24(2, 4);
 	}
+	std::cout << std::endl;
+	std::cout <<"M4: "<< M4<<std::endl;
 
-	std::cout << M4<<std::endl;
+	double *Array = new double[N - 1];
+
+	for (int i = 0; i < N; i++)
+	{
+
+		Array[i] = a + (i + 0.5)*h;
+	}
+
+	double tau = 0.5;
+	double phi0 = (1 + 2 * tau)*(1 - tau)*(1 - tau);
+	double phi1 = tau*(1 - tau)*(1 - tau);
+	double S31;
+	double fi;
+	double fiplus1;
+
+	std::cout << std::endl;
+
+	PrintSplineResultHead();
+	double er = (M4 / 384 + M5 * h / 240)*h*h*h*h;
+	for (int i = 0; i <N-1; i++)
+	{
+		
+		if (variant == 19)
+		{
+			fi = F19(xArray[i],0);
+			fiplus1 = F19(xArray[i + 1],0);
+			f= F19(Array[i],0);
+		}
+		else if (variant == 20)
+		{
+			fi = F20(xArray[i],0);
+			fiplus1 = F20(xArray[i + 1],0);
+			f = F20(Array[i],0);
+		}
+		else if (variant == 24)
+		{
+			fi = F24(Array[i],0);
+			fiplus1 = F24(xArray[i + 1],0);
+			f = F24(Array[i],0);
+		}
+
+		S31 = Phi0(tau)*fi + Phi0(1 - tau)*fiplus1 + h * (Phi1(tau)*m[i] - Phi1(1 - tau)*m[i + 1]);
+
+		PrintSplineResultBody(xArray[i], f, S31, abs(S31 - f), er);
+
+	}
+
+}
+
+double** Interpolation::InitDifTable(int n)
+{
+	double** difTable = new double*[n + 1];
+	for (int i = 0; i < n + 1; i++)
+	{
+		difTable[i] = new double[n + 2];
+		
+	}
+
+	for (int i = 0; i < n + 1; i++)
+		for (int j = 0; j < n + 2; j++)
+			difTable[i][j] = 0;
+	return difTable;
+}
+
+void Interpolation::ReversedInterpolation(double* xArray, double* difVector,double** difTable,int n,int variant)
+{
+	std::cout << std::endl;
+
+	double* yArray = new double[n + 1];
+
+	for (int i = 0; i < n + 1; i++)
+	{
+	
+
+		if (variant == 20)
+		{
+			yArray[i] = F20(xArray[i], c20);
+		}
+		else if (variant == 24)
+		{
+			yArray[i] = F24(xArray[i], c24);
+		}
+		else if (variant == 19)
+		{
+			yArray[i] = F19(xArray[i], c19);
+
+		}
+
+	}
+
+
+
+
+	MakeDifTable(difTable, xArray,yArray, difVector, variant);
+
+	double x = difTable[0][1];
+	double w = 1;
+	double c = 1;
+	double nevyazka = 0;
+
+	for (int i = 0; i < n + 1; i++)
+	{
+
+
+		if (variant == 20)
+		{
+			yArray[i] = F20(xArray[i], 0);
+		}
+		else if (variant == 24)
+		{
+			yArray[i] = F24(xArray[i], 0);
+		}
+		else if (variant == 19)
+		{
+			yArray[i] = F19(xArray[i], 0);
+
+		}
+
+	}
+
+	/*if (variant==19)
+		c = F19((b-a)/2,0);
+	else if (variant==20)
+		c = F20((b - a) / 2, 0);
+	else if (variant ==24)
+		c = F24((b - a) / 2, 0);*/
+
+	for (int i = 1; i <= n; i++)
+	{
+		w *= c - yArray[i - 1];
+		x += difTable[0][i + 1] * w;
+	}
+	
+	if (variant == 19)
+		nevyazka = F19(x,0) - c;
+	else if (variant == 20)
+		nevyazka = F20(x, 0) - c;
+	else if (variant == 24)
+		nevyazka = F24(x, 0) - c;
+
+	std::cout << "\nКорень: " << x << std::endl;
+	std::cout << "Невязка: " << std::setprecision(8) <<abs(nevyazka) << std::endl;
 
 
 }
--- Main.cpp
+++ Main.cpp
@@ -11,9 +11,29 @@ using namespace std;
 
 int main()
 {
+	double a = 1;
+	double b = 2;
+	double h = 0.2;
+	int n = 5;
+	double* xArray = new double[n + 1];
+	for (int i = 0; i < n + 1; i++)
+		xArray[i] = a + i * h;
 	
 
-	Interpolation::MakeDifTable(1, 2, 0.2,19);
+	double** difTable = Interpolation::InitDifTable(n);//del
+	double* difVector = new double[n + 1];
 	
+
+	Interpolation::MakeDifTable(difTable,xArray,difVector,19);
+	Interpolation::NewtonInterpolation(difVector, n + 1, 19, a, b, n, xArray, n);
+	Interpolation::CubeSpline(xArray, difVector, n + 1, 19);
+
+	difTable = Interpolation::InitDifTable(n);
+	difVector = new double[n + 1];
+	Interpolation::ReversedInterpolation(xArray, difVector, difTable, n, 19);
+
+
+
+
 	return 0;
 }
